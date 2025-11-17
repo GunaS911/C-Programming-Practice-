f69188abc5b70f5d86bfd8b055cfd7222a5f8ae4
GunaS911 <gunaswaminathan2567@gmail.com>
2025-11-17 21:34:33 +0530

    Add files via upload

---
+++ countnum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+	int n;
+	printf("Enter your number to count: ");
+	scanf("%d",&n);
+	int i =n;
+	while (i>=0){
+	       printf("%d \n",i);
+       	       i=i-1;
+	}
+return 0;
+}
